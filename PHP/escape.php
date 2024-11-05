04e8880841cfcc75a60f3f7a099aec30898fbe74
MIPS32r2 <nordestino06@gmail.com>
2024-11-05 18:04:41 -0300

    Aprendizado

---
+++ PHP/escape.php
@@ -0,0 +1,5 @@
+<?php if ($expression == true): ?>
+ Isso irá aparecer se a expressão for verdadeira.
+<?php else: ?>
+ Senão isso irá aparecer.
+<?php endif; ?>
---
+++ comentarios.php
@@ -0,0 +1,7 @@
+<?php 
+	echo 'Isto é um teste'; // Este é um comentário no estilo C++ de uma linha
+	/* Este é um comentário de várias linhas
+	   mais mais linha de comentário */
+	echo 'Este é mais um teste';
+	echo 'Um teste final'; #Este é um comentário no estilo shell de uma linha
+?>
