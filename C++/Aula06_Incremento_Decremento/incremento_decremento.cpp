5c6ee1d9216dd9e21a1fb5ce3ae145369f6e7601
Josue da Silva Feitosa Junior <josue.feitosa@goias.gov.br>
2024-12-20 18:52:29 -0300

    Incremento e Decremento de Variáveis, Operadores pós e pré-fixados

---
+++ C++/Aula06_Incremento_Decremento/incremento_decremento.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int n1, n2;
+
+  n1=0;
+  n2=10;
+
+  cout << n1 << "\n\n"; //Atribui inicialmente o valor 0
+
+  //n1=n1+1;
+  n1+=1;
+  cout << n1 << "\n\n";
+
+  return 0;
+}
