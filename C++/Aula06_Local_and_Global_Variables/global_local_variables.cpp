dd3e11156eb687b87562a712873604ecb395a1c1
MIPS32r2 <nordestino06@gmail.com>
2024-12-20 23:21:38 -0300

    Variáveis Locais e Globais

---
+++ C++/Aula06_Local_and_Global_Variables/global_local_variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int n1, n2; // Variáveis Globais
+
+int main(){
+
+  //Operadores Matemáticos: + - / * % ()
+
+  int n3, n4; // Variáveis Locais
+  int res;
+
+  n1=11;
+  n2=3;
+  n3=5;
+  n4=2;
+
+
+  res=n1+n2+n3+n4;
+  cout << "A soma de todas as variáveis: " << res << "\n\n";
+
+  return 0;
+}
