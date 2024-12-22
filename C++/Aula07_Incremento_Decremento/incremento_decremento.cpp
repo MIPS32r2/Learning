79f968f6b11fcf6eb0a3e9664d50347b37e63497
MIPS32r2 <nordestino06@gmail.com>
2024-12-21 23:52:27 -0300

    Incremento e Decremento de Variáveis, Pré e Pós fixadas

--- C++/Aula07_Incremento_Decremento/incremento_decremento.cpp
+++ C++/Aula07_Incremento_Decremento/incremento_decremento.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main(){
 
-  int n1, n2;
+  //n1=n1+1; forma padrão de incremento de uma variável
+  // n1-=1; forma contraída, pode-se usar qualquer operador
+  //n1=n1-10      n1-=10;
+  //n1=n1*10;     n1*=10;
+  //n1=n1/10;     n1/=10;
+  ////n1++; adiciona 1 ou se for n1-- subtrai 1. incrementa ou decrementa apenas 1 
+  //n1++ pós-fixado utiliza o valor depois incrementa ou decrementa
+  //++n1 pré-fixado incrementa/decrementa antes de realizar a operação
 
-  n1=0;
-  n2=10;
 
-  cout << n1 << "\n\n"; //Atribui inicialmente o valor 0
+  int n1=10;
 
-  //n1=n1+1;
-  n1+=1;
+  cout << n1-- << "\n\n";
   cout << n1 << "\n\n";
 
   return 0;
