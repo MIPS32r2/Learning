91e3777552114577ad41223023522780d1990e10
MIPS32r2 <nordestino06@gmail.com>
2024-12-23 14:59:20 -0300

    melhora do algoritmo

--- C++/Exercises/soma.cpp
+++ C++/Exercises/soma.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -7,16 +8,41 @@ int main(){
 
   int n1,n2,resultado;
   
+
+  parte_1:
+
   system("clear");
 
   cout << "Digite um número inteiro: ";
   cin >> n1;
+
+  //Verificando a entrada errada
+  if(cin.fail()){
+    cout << "Valor incorreto, favor inserir um número inteiro" << "\n";
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); //Ignora o restante da linha 
+    goto parte_1;
+  }
+
+  system("clear");
+
+  parte_2:
+
   cout <<  "Digite outro número inteiro: ";
   cin >> n2;
+  
+  if(cin.fail()){
+    cout << "Valor incorreto, favor inserir um número inteiro" << "\n";
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    goto parte_2;
+  }
+  
+  system("clear");
 
   resultado=n1+n2;
 
-  cout << "O Resultado da soma é: " << resultado << "\n";
+  cout << "\nO Resultado da soma é: " << resultado << "\n";
     
   return 0;
 }
