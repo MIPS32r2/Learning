67414e4628fe1060f24e14209066e14734438ba6
MIPS32r2 <nordestino06@gmail.com>
2024-12-25 18:10:01 -0300

    Operadores And,not e or

---
+++ C++/Aula11_Operadores_AND_NOT_OR/operadores.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int num;
+
+
+  /*
+  1 2 3 4 5 6 7 8 9 10 
+  >4 e <7
+   */
+
+
+  /*num=9;
+
+  if(num < 3 || num > 8){
+    cout << "\n\nValor aceito\n";
+  }else{
+    cout << "\n\nValor não aceito\n";
+  }
+*/
+
+num=1;
+
+if(num){
+  cout << "\n\nVou ao clube\n";
+}else{
+    cout << "\n\nVou ao cinema\n";
+}
+
+
+
+
+  return 0;
+}
