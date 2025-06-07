71e287e3b04e05d75d4703bdab87cb907e0aecdb
MIPS32r2 <nordestino06@gmail.com>
2025-06-06 23:58:00 -0300

    Entrada e Saída de Dados em C

---
+++ C/AEDS/02-entrada-saida/IO.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main (){
+    int var1, var2;
+
+    printf("\ndigite o primeiro número: ");
+    scanf("%d",&var1);
+
+    printf("\nDigite o segundo número: ");
+    scanf("%d",&var2);
+
+    printf("\nOs números digitados foram: %d e %d \n", var1,var2);
+    return 0;
+}
